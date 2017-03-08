e0dd3bd4ef7d735df6d5403b267afb94bf43b7e3
thomas <sketchbang@gmail.com>
2017-03-07 21:59:36 -0600

    working on styling

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 import './style.scss';
 
-var nav = 0;
+(function() {
+  var navState = 0;
+  document.getElementById('open').addEventListener("click", toggleNav);
 
-document.getElementById('open').addEventListener("click", toggleNav);
-
-function toggleNav() {
-  console.log(nav, 'here');
-  const button = document.getElementById('open');
-  if (nav === 1) {
-    nav = 0;
-    button.classList.remove('is-active');
-  } else {
-    nav = 1;
-    button.classList.add('is-active');
+  function toggleNav() {
+    console.log('here');
+    var button = document.getElementById('open');
+    var nav = document.getElementById('nav');
+    if (navState === 1) {
+      navState = 0;
+      button.classList.remove('is-active');
+      nav.style.width = 0;
+    } else {
+      navState = 1;
+      button.classList.add('is-active');
+      nav.style.width = '100%';
+    }
   }
-}
+})();
