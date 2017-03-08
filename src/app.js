98a64b0cb3379860987a787b336fa76a3074ac48
thomas <sketchbang@gmail.com>
2017-03-07 20:46:39 -0600

    fix hamburger

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 import './style.scss';
+
+var nav = 0;
+
+document.getElementById('open').addEventListener("click", toggleNav);
+
+function toggleNav() {
+  console.log(nav, 'here');
+  const button = document.getElementById('open');
+  if (nav === 1) {
+    nav = 0;
+    button.classList.remove('is-active');
+  } else {
+    nav = 1;
+    button.classList.add('is-active');
+  }
+}
