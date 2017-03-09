69fcaa080766ea6bb337f2c12cadcc75c80d8b49
thomas <sketchbang@gmail.com>
2017-03-08 21:35:29 -0600

    style

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import './style.scss';
 
 (function() {
+  var accent = '#4666a7';
+  var light = '#fafafa';
+
   var navState = 0;
   document.getElementById('open').addEventListener("click", toggleNav);
 
@@ -8,14 +11,17 @@ import './style.scss';
     console.log('here');
     var button = document.getElementById('open');
     var nav = document.getElementById('nav');
+    var left = document.getElementById('left');
     if (navState === 1) {
       navState = 0;
       button.classList.remove('is-active');
       nav.style.width = 0;
+      left.style.color = light;
     } else {
       navState = 1;
       button.classList.add('is-active');
       nav.style.width = '100%';
+      left.style.color = accent;
     }
   }
 })();
